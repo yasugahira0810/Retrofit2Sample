340a80e61924fbe627827725284daa9bbd63ef85
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2018-08-16 13:19:26 +0900

    Create 2 sub activities.

--- app/src/main/java/com/example/retrofit2sample/MainActivity.java
+++ app/src/main/java/com/example/retrofit2sample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.retrofit2sample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bt_showTags = (Button)findViewById(R.id.button_show_tags);
+
+        bt_showTags.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClassName(getPackageName(), getPackageName() + ".ShowTagsActivity");
+                startActivity(intent);
+            }
+        });
+
+        Button bt_postArticle = (Button)findViewById(R.id.button_post_article);
+
+        bt_postArticle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClassName(getPackageName(), getPackageName() + ".PostArticleActivity");
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/retrofit2sample/PostArticleActivity.java
@@ -0,0 +1,13 @@
+package com.example.retrofit2sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class PostArticleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_article);
+    }
+}
