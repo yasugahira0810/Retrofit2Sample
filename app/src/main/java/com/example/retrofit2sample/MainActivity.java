85fa10a0b398a9c8ea879bc1f7f28e7075811aff
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2018-08-16 20:23:56 +0900

    Fix bug.

--- app/src/main/java/com/example/retrofit2sample/MainActivity.java
+++ app/src/main/java/com/example/retrofit2sample/MainActivity.java
@@ -31,32 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
         Button bt_showTags = (Button) findViewById(R.id.button_show_tags);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://qiita.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
 
-        TagService service = retrofit.create(TagService.class);
 
-        Call<List<Tag>> tags = service.ListTags();
 
-        tags.enqueue(new Callback<List<Tag>>() {
-            @Override
-            public void onResponse(Call<List<Tag>> call, Response<List<Tag>> response) {
-                List<Tag> listTag = response.body();
-                int s = listTag.size();
-                for (int i = 0; i < s; i++) {
-                    Log.d("debug3", listTag.get(i).getId());
-                    followTags.add(listTag.get(i).getId());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Tag>> call, Throwable t) {
-                Log.d("debug4", t.getMessage());
-            }
-
-        });
 
         // 同期処理のexecuteではダメだった。一旦事例の多いenqueueで実装してみる
 //                try {
@@ -74,12 +51,41 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent();
-                intent.setClassName(getPackageName(), getPackageName() + ".ShowTagsActivity");
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("https://qiita.com/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
 
-                intent.putStringArrayListExtra("followTags", followTags);
 
-                startActivity(intent);
+                TagService service = retrofit.create(TagService.class);
+
+                Call<List<Tag>> tags = service.ListTags();
+
+                tags.enqueue(new Callback<List<Tag>>() {
+                    @Override
+                    public void onResponse(Call<List<Tag>> call, Response<List<Tag>> response) {
+                        List<Tag> listTag = response.body();
+                        int s = listTag.size();
+                        for (int i = 0; i < s; i++) {
+                            Log.d("debug3", listTag.get(i).getId());
+                            followTags.add(listTag.get(i).getId());
+                        }
+
+                        Intent intent = new Intent();
+                        intent.setClassName(getPackageName(), getPackageName() + ".ShowTagsActivity");
+
+                        intent.putStringArrayListExtra("followTags", followTags);
+
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Tag>> call, Throwable t) {
+                        Log.d("debug4", t.getMessage());
+                    }
+
+                });
+
             }
         });
 
